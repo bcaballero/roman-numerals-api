10d946282b94f4a541edc74360b887cc770ab7de
Brian Caballero <brian.caballero@kaplan.edu.au>
2021-11-19 00:45:43 +0800

    Add convert to roman numeral helper function

---
+++ app/helpers.php
@@ -0,0 +1,40 @@
+<?php
+
+if(!function_exists('convert_roman'))
+{
+    function convert_roman ($number)
+    {
+        $map = [
+            'M' => 1000,
+            'CM' => 900,
+            'D' => 500,
+            'CD' => 400,
+            'C' => 100,
+            'XC' => 90,
+            'L' => 50,
+            'XL' => 40,
+            'X' => 10,
+            'IX' => 9,
+            'V' => 5,
+            'IV' => 4,
+            'I' => 1
+        ];
+        
+        $roman_numeral = '';
+
+        while ($number > 0)
+        {
+            foreach ($map as $roman => $int)
+            {
+                if($number >= $int)
+                {
+                    $number -= $int;
+                    $roman_numeral .= $roman;
+                    break;
+                }
+            }
+        }
+        
+        return $roman_numeral;
+    }
+}
